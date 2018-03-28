a342375828033d8d53becc47a093c53ce018e860
Luc Giffon <luc.giffon@lif.univ-mrs.fr>
2018-03-28 08:47:29 +0200

    gitignore + change in vgg19_keras: now use skluc.mldataset for loading dataset + use validation size of 10000 and seed at 0 + change datapreprocessing from raw changes to the normalize method + doesn't load imagenet weight before training

--- 3_Vgg19_Network/Vgg19_keras.py
+++ 3_Vgg19_Network/Vgg19_keras.py
@@ -8,8 +8,9 @@
 # ========================================================== #
 
 import keras
+import time
 import numpy as np
-from keras.datasets import cifar10
+# from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -18,11 +19,12 @@ from keras.initializers import he_normal
 from keras import optimizers
 from keras.callbacks import LearningRateScheduler, TensorBoard
 from keras.layers.normalization import BatchNormalization
-from keras.utils.data_utils import get_file
+# from keras.utils.data_utils import get_file
+from skluc.mldatasets import Cifar10Dataset as dataset
 
 num_classes  = 10
 batch_size   = 128
-epochs       = 200
+epochs       = 500
 iterations   = 391
 dropout      = 0.5
 weight_decay = 0.0001
@@ -35,23 +37,35 @@ def scheduler(epoch):
         return 0.01
     return 0.001
 
-WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels.h5'
-filepath = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5', WEIGHTS_PATH, cache_subdir='models')
+# WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels.h5'
+# filepath = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5', WEIGHTS_PATH, cache_subdir='models')
 
 # data loading
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+
+data = dataset.Cifar10Dataset(validation_size=10000, seed=0)
+
+data.load()
+data.normalize()
+data.data_astype(np.float32)
+data.labels_astype(np.float32)
+data.to_one_hot()
+data.flatten()
+
+(x_train, y_train), (x_test, y_test) = data.train, data.test
+
 
 # data preprocessing 
-x_train[:,:,:,0] = (x_train[:,:,:,0]-123.680)
-x_train[:,:,:,1] = (x_train[:,:,:,1]-116.779)
-x_train[:,:,:,2] = (x_train[:,:,:,2]-103.939)
-x_test[:,:,:,0] = (x_test[:,:,:,0]-123.680)
-x_test[:,:,:,1] = (x_test[:,:,:,1]-116.779)
-x_test[:,:,:,2] = (x_test[:,:,:,2]-103.939)
+# x_train[:,:,:,0] = (x_train[:,:,:,0]-123.680)
+# x_train[:,:,:,1] = (x_train[:,:,:,1]-116.779)
+# x_train[:,:,:,2] = (x_train[:,:,:,2]-103.939)
+# x_test[:,:,:,0] = (x_test[:,:,:,0]-123.680)
+# x_test[:,:,:,1] = (x_test[:,:,:,1]-116.779)
+# x_test[:,:,:,2] = (x_test[:,:,:,2]-103.939)
 
 # build model
 model = Sequential()
@@ -134,7 +148,7 @@ model.add(BatchNormalization())
 model.add(Activation('softmax'))
 
 # load pretrained weight from VGG19 by name      
-model.load_weights(filepath, by_name=True)
+# model.load_weights(filepath, by_name=True)
 
 # -------- optimizer setting -------- #
 sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True)
@@ -142,7 +156,7 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy
 
 tb_cb = TensorBoard(log_dir=log_filepath, histogram_freq=0)
 change_lr = LearningRateScheduler(scheduler)
-cbks = [change_lr,tb_cb]
+cbks = [change_lr, tb_cb]
 
 print('Using real-time data augmentation.')
 datagen = ImageDataGenerator(horizontal_flip=True,
@@ -155,4 +169,7 @@ model.fit_generator(datagen.flow(x_train, y_train,batch_size=batch_size),
                     epochs=epochs,
                     callbacks=cbks,
                     validation_data=(x_test, y_test))
-model.save('retrain.h5')
+
+model.save('{}_vgg19_cifar10'.format(time.time()))
+
+print("Final evaluation on test set: {}".format(model.evaluate(x_test, y_test)))
